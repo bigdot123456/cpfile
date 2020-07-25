4a556398fb5c3af1d89c8b387e8c395737444ed6
bigdot123456 <bigdot@126.com>
2020-07-25 08:56:20 +0800

    add initial copy python file

---
+++ cpfiles.py
@@ -0,0 +1,136 @@
+import os
+import shutil
+import sys
+
+m0list = {}
+
+m0list["tb"] = [
+    "../debug_tester/verilog/cortexm0_debug_tester.v",
+    "../debug_tester/verilog/cortexm0_debug_tester_rom.v",
+    "../debug_tester/verilog/cortexm0_debug_tester_ahb_sram_bridge.v",
+    "../debug_tester/verilog/cortexm0_debug_tester_sram.v",
+    "../debug_tester/verilog/cortexm0_debug_tester_ahb_interconnect.v",
+    "../debug_tester/verilog/cortexm0_debug_tester_ahb_def_slv.v",
+    "../debug_tester/verilog/cortexm0_debug_tester_io.v",
+]
+
+m0list["tb2"] = [
+    "../verilog/tb_clkreset.v",
+    "../verilog/tb_uart_capture.v",
+]
+
+m0list["rtl_top"] = ["../verilog/cortex_m0_mcu.v"]
+
+m0list["m0_top"] = [
+    "../verilog/cortex_m0_mcu_clkctrl.v",
+    "../verilog/cortex_m0_mcu_system.v",
+    "../verilog/cortex_m0_mcu_pin_mux.v",
+    "../verilog/cortex_m0_mcu_sysctrl.v",
+    "../verilog/cortex_m0_mcu_stclkctrl.v",
+]
+
+m0list["mem"] = [
+    "../../../logical/models/clkgate/mcu_clock_gate.v",
+    "../../../logical/models/memories//ahb_rom.v",
+    "../../../logical/models/memories//ahb_ram.v",
+    "../../../logical/models/memories//ahb_ram_beh.v",
+    "../../../logical/models/memories//fpga_rom.v",
+    "../../../logical/models/memories//flash_rom32.v",
+    "../../../logical/models/memories//fpga_sram.v",
+    "../../../logical/models/memories//sram256x16.v",
+    "../../../logical/models/memories//sram256x8.v",
+]
+
+m0list["amba"] = [
+    "../../../logical/apb_subsystem/verilog/apb_subsystem.v",
+    "../../../logical/apb_subsystem/verilog/apb_test_slave.v",
+    "../../../logical/apb_subsystem/verilog/irq_sync.v",
+    "../../../logical/ahb_slave_mux/verilog/ahb_slave_mux.v",
+    "../../../logical/ahb_master_mux/verilog/ahb_master_mux.v",
+    "../../../logical/ahb_default_slave/verilog/ahb_default_slave.v",
+    "../../../logical/ahb_gpio/verilog/ahb_gpio.v",
+    "../../../logical/ahb_to_apb/verilog/ahb_to_apb.v",
+    "../../../logical/ahb_bitband/verilog/ahb_bitband.v",
+    "../../../logical/ahb_to_sram/verilog/ahb_to_sram.v",
+    "../../../logical/ahb_to_flash32/verilog/ahb_to_flash32.v",
+    "../../../logical/ahb_to_extmem16/verilog/ahb_to_extmem16.v",
+    "../../../logical/ahb_to_extmem16/verilog/ahb_to_extmem16_ahb_fsm.v",
+    "../../../logical/ahb_to_extmem16/verilog/ahb_to_extmem16_mem_fsm.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0_integration/verilog/CORTEXM0INTEGRATION.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0_integration/verilog/cortexm0_pmu.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0_integration/verilog/cortexm0_wic.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/models/cells/cm0_dbg_reset_sync.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/models/cells/cm0_pmu_sync_set.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/models/cells/cm0_pmu_sync_reset.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/models/cells/cm0_pmu_cdc_send_reset.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/models/cells/cm0_pmu_cdc_send_set.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/models/cells/cm0_pmu_acg.v",
+    "../../../logical/apb_timer/verilog/apb_timer.v",
+    "../../../logical/apb_dualtimers/verilog/apb_dualtimers.v",
+    "../../../logical/apb_dualtimers/verilog/apb_dualtimers_frc.v",
+    "../../../logical/apb_uart/verilog/apb_uart.v",
+    "../../../logical/apb_watchdog/verilog/apb_watchdog.v",
+    "../../../logical/apb_watchdog/verilog/apb_watchdog_frc.v",
+    "../../../logical/apb_slave_mux/verilog/apb_slave_mux.v",
+]
+
+m0list["m0_rtl"] = [
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/CORTEXM0.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_top.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_top_sys.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_top_dbg.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_top_clk.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_core.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_nvic.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_matrix.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_dbg_ctl.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_dbg_bpu.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_dbg_dwt.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_dbg_if.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_dbg_sel.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_core_alu.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_core_spu.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_core_mul.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_core_psr.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_core_gpr.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_core_pfu.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_core_ctl.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_nvic_reg.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_nvic_main.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_matrix_sel.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0/verilog/cm0_core_dec.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0_dap/verilog/CORTEXM0DAP.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0_dap/verilog/cm0_dap_dp.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0_dap/verilog/cm0_dap_ap.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0_dap/verilog/cm0_dap_dp_jtag.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0_dap/verilog/cm0_dap_dp_sw.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0_dap/verilog/cm0_dap_dp_cdc.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0_dap/verilog/cm0_dap_dp_pwr.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0_dap/verilog/cm0_dap_ap_mast.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/cortexm0_dap/verilog/cm0_dap_ap_cdc.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/models/cells/cm0_acg.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/models/cells/cm0_dap_jt_cdc_comb_and.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/models/cells/cm0_dap_sw_cdc_capt_reset.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/models/cells/cm0_dap_cdc_capt_sync.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/models/cells/cm0_dap_cdc_send_reset.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/models/cells/cm0_dap_cdc_send_data.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/models/cells/cm0_dap_cdc_send_addr.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/models/cells/cm0_dap_cdc_send.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/models/cells/cm0_dap_cdc_comb_and_data.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/models/cells/cm0_dap_cdc_comb_and.v",
+    "../../../cores/AT510-BU-50000-r0p0-02rel0/logical/models/cells/cm0_dap_cdc_comb_and_addr.v",
+]
+
+for i in m0list.keys():
+    if not os.path.exists(i):
+        os.makedirs(i)
+    rtlList = m0list[i]
+    for j in rtlList:
+        print(f"cp {j} {i}\n")
+        # adding exception handling
+        try:
+            shutil.copy(j, i)
+        except IOError as e:
+            print("Unable to copy file. %s" % e)
+        except:
+            print("Unexpected error:", sys.exc_info())
